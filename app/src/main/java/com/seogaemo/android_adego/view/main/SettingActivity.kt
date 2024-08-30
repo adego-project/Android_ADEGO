6daec6a0bded2e233fece44cd76878a0cf9b56b3
JangWoojun <woojun0107@naver.com>
2024-08-30 16:28:41 +0900

    feat: 약속 탈퇴 시 서비스 해제 추가

--- app/src/main/java/com/seogaemo/android_adego/view/main/SettingActivity.kt
+++ app/src/main/java/com/seogaemo/android_adego/view/main/SettingActivity.kt
@@ -13,13 +13,13 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.seogaemo.android_adego.R
 import com.seogaemo.android_adego.data.ImageRequest
-import com.seogaemo.android_adego.data.PlanResponse
 import com.seogaemo.android_adego.data.UserResponse
 import com.seogaemo.android_adego.database.SharedPreference
 import com.seogaemo.android_adego.database.TokenManager
 import com.seogaemo.android_adego.databinding.ActivitySettingBinding
 import com.seogaemo.android_adego.network.RetrofitAPI
 import com.seogaemo.android_adego.network.RetrofitClient
+import com.seogaemo.android_adego.service.BackgroundLocationUpdateService
 import com.seogaemo.android_adego.util.Util
 import com.seogaemo.android_adego.util.Util.createDialog
 import com.seogaemo.android_adego.util.Util.getUser
@@ -77,6 +77,8 @@ class SettingActivity : AppCompatActivity() {
                     CoroutineScope(Dispatchers.IO).launch {
                         leavePlan(this@SettingActivity)
                         withContext(Dispatchers.Main) {
+                            val intent = Intent(this@SettingActivity, BackgroundLocationUpdateService::class.java)
+                            stopService(intent)
                             Toast.makeText(this@SettingActivity, "약속에서 탈퇴하셨습니다", Toast.LENGTH_SHORT).show()
                             dialog.dismiss()
                         }
